a8cbcfe2968af91246634f3436884a0953d2ac54
jvafonso32-png <jvafonso32@gmail.com>
2026-02-10 23:45:44 +0000

    supertrunfadv

---
+++ supertrunfadv.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+
+/*
+  Desafio Super Trunfo - Países
+  Nível: Iniciante
+  Objetivo: Cadastro e exibição de duas cartas de cidades.
+*/
+
+int main() {
+    // --- Variáveis para a Carta 1 ---
+    char estado1;
+    char codigo1[4];
+    char cidade1[50];
+    unsigned long int populacao1;
+    float area1;
+    float pib1;
+    int pontosTuristicos1;
+    float densidadepopulacional1;
+    float pibpercapta1;
+    float superpoder1;
+
+    // --- Variáveis para a Carta 2 ---
+    char estado2;
+    char codigo2[4];
+    char cidade2[50];
+    unsigned long int populacao2;
+    float area2;
+    float pib2;
+    int pontosturisticos2;
+    float densidadepopulacional2;
+    float pibpercapta2;
+    float superpoder2;
+
+
+    //---ENTRADA DE DADOS----
+    printf("Estado entre A--H\n");
+    scanf("%c",&estado1);
+    printf("Codigo EX A01 \n");
+    scanf("%s", codigo1);
+    printf("Cidade\n");
+    scanf(" %[^\n]s", cidade1);
+    printf("Populacao\n");
+    scanf("%lu", &populacao1);
+    printf("Area\n");
+    scanf("%f",&area1);
+    printf("Pib\n");
+    scanf("%f",&pib1);
+    printf("Pontos turisticos\n");
+    scanf("%d",&pontosTuristicos1);
+
+    //---ENTRADA DE DADOS 2----
+    printf("Estado entre A--H\n");
+    scanf(" %c",&estado2);
+    printf("Codigo AO2 \n");
+    scanf("%s", codigo2);
+    printf("Cidade\n");
+    scanf(" %[^\n]s", cidade2);
+    printf("Populacao\n");
+    scanf("%lu", &populacao2);
+    printf("Area\n");
+    scanf("%f",&area2);
+    printf("Pib\n");
+    scanf("%f",&pib2);
+    printf("Pontos turisticos\n");
+    scanf("%d",&pontosturisticos2);
+    //---RESPOSTA PC 1----
+    printf("\n----------------------\n");
+    printf("carta 1 \n");
+    printf(" estado- %c \n",estado1);
+    printf(" codigo- %s \n", codigo1);
+    printf(" cidade- %s \n", cidade1);
+    printf(" populacao- %lu \n", populacao1);
+    printf(" area- %.2f \n", area1);
+    printf(" pib-%.2f \n",pib1);
+    printf(" pontos turisticos- %d \n",pontosTuristicos1);
+    densidadepopulacional1 = (populacao1 / area1);
+    printf("desidade populacional é: %f\n", densidadepopulacional1);
+    pibpercapta1 = (pib1 / populacao1);
+    printf("o PIB é: %f\n", pibpercapta1);
+    superpoder1 = (float) populacao1 + area1 + (float) pontosTuristicos1 + pib1 + pibpercapta1 + 1.0f  / densidadepopulacional1;
+    printf(" super poder e: %.2f \n\n", superpoder1);
+
+    //---RESPOSTA PC 2---- 
+    printf("\n----------------------\n");
+    printf("carta 2 \n");
+    printf(" estado- %c \n",estado2);
+    printf(" codigo- %s \n", codigo2);
+    printf(" cidade- %s \n", cidade2);
+    printf(" populacao- %lu \n", populacao2);
+    printf(" area- %.2f \n", area2);
+    printf(" pib-%.2f \n",pib2);
+    printf(" pontos turisticos- %d \n",pontosturisticos2);
+    densidadepopulacional2 = (populacao2 / area2);
+    printf("desidade populacional é: %f\n", densidadepopulacional2);
+    pibpercapta2 =(float) (pib2 / populacao2);
+    printf("o PIB é: %f\n", pibpercapta2);
+    superpoder2 = (float) populacao2 + area2 + (float) pontosturisticos2 + pib2 + pibpercapta2 + 1.0f / densidadepopulacional2;
+    printf(" super poder e: %.2f \n\n", superpoder2);
+
+     //---COMPARACAO DAS CARTAS
+    printf("comparacao das cartas \n");
+    printf("Populacao: carta 1 venceu %d \n", populacao1 > populacao2);
+    printf("area carta 1 venceu %d  \n", area1 > area2);
+    printf("pib: carta 1 venceu %d \n", pib1 > pib2 );
+    printf("pontos turisticos: carta 1 venceu %d \n", pontosTuristicos1  > pontosturisticos2);
+    printf("densidade: carta 1 venceu %d \n", densidadepopulacional1 < densidadepopulacional2);
+    printf("pib percapta: carta 1 venceu %d \n", pibpercapta1 > pibpercapta2);
+    printf("superpoder: carta 1 venceu %d \n", superpoder1 > superpoder2);
+
+    
+    
+
+    return 0;
+
+}    
